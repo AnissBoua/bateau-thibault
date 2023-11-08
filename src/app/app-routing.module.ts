8a46903c8778fa177ad0cf0bb2ff7dc8c784812c
Anisse <anissbouainbi@hotmail.it>
2023-11-08 12:24:19 +0100

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./views/home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'tabs/home',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'tabs',
+    loadChildren: () => import('./views/tabs/tabs.module').then( m => m.TabsPageModule)
   },
 ];
 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      'crimson': ['"Crimson Pro"', 'serif'],
+      'open': ['"Open Sans"', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
